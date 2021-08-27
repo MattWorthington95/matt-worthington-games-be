55b71808ff9de6fc1081e9adc9706289b71a35c2
matt <mattworthington51@gmail.com>
2021-08-27 16:42:44 +0100

    changed db pictures

--- db/data/development-data/users.js
+++ db/data/development-data/users.js
@@ -1,36 +1,37 @@
 module.exports = [
   {
-    username: 'tickle122',
-    name: 'Tom Tickle',
+    username: "tickle122",
+    name: "Tom Tickle",
     avatar_url:
-      'https://www.spiritsurfers.net/monastery/wp-content/uploads/_41500270_mrtickle.jpg'
+      "https://static.wikia.nocookie.net/mrmen/images/d/d6/Mr-Tickle-9a.png/revision/latest?cb=20180127221953",
   },
   {
-    username: 'grumpy19',
-    name: 'Paul Grump',
-    avatar_url: 'https://www.tumbit.com/profile-image/4/original/mr-grumpy.jpg'
+    username: "grumpy19",
+    name: "Paul Grump",
+    avatar_url: "https://www.tumbit.com/profile-image/4/original/mr-grumpy.jpg",
   },
   {
-    username: 'happyamy2016',
-    name: 'Amy Happy',
+    username: "happyamy2016",
+    name: "Amy Happy",
     avatar_url:
-      'https://vignette1.wikia.nocookie.net/mrmen/images/7/7f/Mr_Happy.jpg/revision/latest?cb=20140102171729'
+      "https://vignette1.wikia.nocookie.net/mrmen/images/7/7f/Mr_Happy.jpg/revision/latest?cb=20140102171729",
   },
   {
-    username: 'cooljmessy',
-    name: 'Peter Messy',
-    avatar_url: 'https://i.imgur.com/WfX0Neu.jpg'
-  },
-  {
-    username: 'weegembump',
-    name: 'Gemma Bump',
+    username: "cooljmessy",
+    name: "Peter Messy",
     avatar_url:
-      'https://www.upandrunning.co.uk/media/catalog/product/cache/1/image/650x/040ec09b1e35df139433887a97daa66f/m/r/mr-bump.jpg'
+      "https://static.wikia.nocookie.net/mrmen/images/c/c2/Mr-cool-1a.jpg/revision/latest?cb=20170519070512",
   },
   {
-    username: 'jessjelly',
-    name: 'Jess Jelly',
+    username: "weegembump",
+    name: "Gemma Bump",
     avatar_url:
-      'https://s-media-cache-ak0.pinimg.com/564x/39/62/ec/3962eca164e60cf46f979c1f57d4078b.jpg'
-  }
+      "http://2.bp.blogspot.com/-3S77I1xNtks/VRLjypJ6_II/AAAAAAAADm0/eSYkNzrwYLo/s1600/Wiggum_Donut_Crop.jpeg",
+  },
+  {
+    username: "jessjelly",
+    name: "Jess Jelly",
+    avatar_url:
+      "https://s-media-cache-ak0.pinimg.com/564x/39/62/ec/3962eca164e60cf46f979c1f57d4078b.jpg",
+  },
 ];
